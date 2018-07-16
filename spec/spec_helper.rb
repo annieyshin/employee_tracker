a54dde87cb37a86883ac7904a925e7a046094ec8
Annie Shin, Nate Cottle and Will Swanson <nate@>
2018-07-16 10:22:31 -0700

    add lots of files

---
+++ spec/spec_helper.rb
@@ -0,0 +1,16 @@
+ENV['RACK_ENV'] = 'test'
+require('rspec')
+require('pg')
+require('sinatra/activerecord')
+require('employee')
+require('division')
+require('pry')
+
+
+RSpec.configure do |config|
+  config.after(:each) do
+    Employee.all.each do |employee|
+      employee.destroy()
+    end
+  end
+end
